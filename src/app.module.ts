57dd23e08dd0f84859795af868b7584dcafb1130
Felipe Gabriel <felipegabriel1106@outlook.com>
2024-10-08 16:28:38 -0300

    feat(code): Cirando as classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FornecedorModule } from './fornecedor/fornecedor.module';
+import { CategoriaModule } from './categoria/categoria.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ProdutoModule } from './produto/produto.module';
+import { PedidoModule } from './pedido/pedido.module';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [],
+  imports: [FornecedorModule, CategoriaModule, UsuarioModule, ProdutoModule, PedidoModule, ClienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
