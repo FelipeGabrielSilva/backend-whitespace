96d98347a5a1291de71c141b57629e7de97d6ec9
Felipe Gabriel <felipegabriel1106@outlook.com>
2024-10-08 22:23:02 -0300

    feat(cors): tentativa de cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita CORS com a origem específica
+  app.enableCors({
+    origin: 'http://localhost:3000', // Substitua pela URL do seu frontend se for diferente
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
